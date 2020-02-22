64bf35155c824c26b7b28e595049d2c304c7c3a4
Kartikey45 <singhkartikey45@gmail.com>
2020-02-22 11:22:05 +0530

    ADDED:Start position initialized

--- Snake_ladder_simulator.sh
+++ Snake_ladder_simulator.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 
 echo "************************************Welcome to Snake and Ladder simulator************************************"
+
+#VARIABLE AND CONSTANT INITIALIZE
+START_POSITION=0
