c6eeeb942eb2cb85e6264080e51febf6bfb5696c
Kartikey45 <singhkartikey45@gmail.com>
2020-02-22 11:32:42 +0530

    ADDED:Show the Random Dice number

--- Snake_ladder_simulator.sh
+++ Snake_ladder_simulator.sh
@@ -4,3 +4,6 @@ echo "************************************Welcome to Snake and Ladder simulator*
 
 #VARIABLE AND CONSTANT INITIALIZE
 START_POSITION=0
+randomDice=$(( RANDOM%6+1 ))
+
+echo $randomDice
