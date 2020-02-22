070024c195d8f6935d93bf62c4c9bbfdf734f7c4
Kartikey45 <singhkartikey45@gmail.com>
2020-02-22 12:53:27 +0530

    ADDED:Game of two player

--- Snake_ladder_simulator.sh
+++ Snake_ladder_simulator.sh
@@ -2,64 +2,92 @@
 
 #VARIABLE AND CONSTANT INITIALIZED
 START_POSITION=0
-playerPosition=0
+player1Position=0
+player2Position=0
 END_POSITION=100
-count=0
 
 #PLAY THE GAME TILL THE END POSITION
-while [ $playerPosition -ne $END_POSITION  ]
+while [ $player1Position -ne $END_POSITION -a $player2Position -ne $END_POSITION ]
 do
 
 	#GENERATE RANDOM VARIABLES
-	randomDiceNumber=$(( RANDOM%6+1 ))
+	randomDiceNumber1=$(( RANDOM%6+1 ))
+	randomDiceNumber2=$(( RANDOM%6+1 ))
 	randomCheck=$(( RANDOM%3 ))
 
 	#CHECK THE CONDITIONS
 	case $randomCheck in
 		1)
-			(( count++ ))
 			echo "Ladder"
-			echo "dice no.: $randomDiceNumber"
-			playerPosition=$(($playerPosition+$randomDiceNumber))
-			if [ $playerPosition -gt $END_POSITION ]
+			echo "dice1 no.: $randomDiceNumber1"
+			echo "dice2 no.: $randomDiceNumber2"
+
+			player1Position=$(($player1Position+$randomDiceNumber1))
+			player2Position=$(($player2Position+$randomDiceNumber2))
+
+			if [ $player1Position -gt $END_POSITION ]
 			then
-				playerPosition=$(($playerPosition-$randomDiceNumber))
-			elif [ $playerPosition -eq $END_POSITION ]
+				player1Position=$(($player1Position-$randomDiceNumber1))
+			elif [ $player1Position -eq $END_POSITION ]
 			then
-				echo "Number of times the dice was played: $count"
-				echo "Player position: $playerPosition"
-				echo "Player Won !!!!!!"
+				echo "Player1 position: $player1Position"
+				echo "Player1 Won !!!!!!"
 				break
 			fi
-			echo "Number of times the dice was played: $count"
-			echo "Player position: $playerPosition"
+			echo "Player1 position: $player1Position"
+
+			if [ $player2Position -gt $END_POSITION ]
+			then
+				player2Position=$(($player2Position-$randomDiceNumber2))
+			elif [ $player2Position -eq $END_POSITION ]
+			then
+				echo "Player2 position: $player2Position"
+				echo "Player2 Won !!!!!!"
+				break
+			fi
+			echo "Player2 position: $player2Position"
 			echo ""
 			;;
 		2)
-			(( count++ ))
 			echo "Snake"
-			echo "dice no.: $randomDiceNumber"
-			playerPosition=$(($playerPosition-$randomDiceNumber))
-			if [ $playerPosition -lt $START_POSITION ]
+			echo "dice1 no.: $randomDiceNumber1"
+			echo "dice2 no.: $randomDiceNumber2"
+
+			player1Position=$(($player1Position-$randomDiceNumber1))
+			player2Position=$(($player2Position-$randomDiceNumber2))
+
+			if [ $player1Position -lt $START_POSITION ]
 			then
-				playerPosition=$START_POSITION
-				echo "Player position: $playerPosition"
+				player1Position=$START_POSITION
+				echo "Player1 position: $player1Position"
 			fi
-			echo "Number of times the dice was played: $count"
-			echo "Player position: $playerPosition"
+			echo "Player1 position: $player1Position"
+
+			if [ $player2Position -lt $START_POSITION ]
+			then
+				player2Position=$START_POSITION
+				echo "player2 position: $player2Position"
+			fi
+			echo "player2 position: $player2Position"
 			echo ""
 			;;
 		*)
 			echo "No play"
-			playerPosition=$playerPosition 
-			echo "Player position: $playerPosition"
+			player1Position=$player1Position
+			player2Position=$player2Position
+			echo "Player1 position: $player1Position"
+			echo "Player2 position: $player2Position"
 			echo ""
 			;;
 	esac
-		
+
 	#CONDITION CHECK FOR NEGETIVE POSITION OF THE PLAYERS
-	if [ $playerPosition -lt $START_POSITION ]
+	if [ $player1Position -lt $START_POSITION ]
 	then
-		playerPosition=$START_POSITION
+		player1Position=$START_POSITION
+	fi
+	if [ $player2Position -lt $START_POSITION ]
+	then
+		player2Position=$START_POSITION
 	fi
 done
