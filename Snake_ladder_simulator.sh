5d1234ac9810df6273c77a3b19241df488208f9b
Kartikey45 <singhkartikey45@gmail.com>
2020-02-22 11:41:08 +0530

    ADDED:Player then check for options-no play, ladder, snake

--- Snake_ladder_simulator.sh
+++ Snake_ladder_simulator.sh
@@ -2,8 +2,29 @@
 
 echo "************************************Welcome to Snake and Ladder simulator************************************"
 
-#VARIABLE AND CONSTANT INITIALIZE
+#VARIABLE AND CONSTANT INITIALIZED
 START_POSITION=0
-randomDice=$(( RANDOM%6+1 ))
+randomDiceNumber=$(( RANDOM%6+1))
+randomCheck=$(( RANDOM%3 ))
+playerPosition=0
 
-echo $randomDice
+#CHECK THE CONDITIONS
+case $randomCheck in
+	1)
+		echo "Ladder"
+		echo "dice no.: $randomDiceNumber"
+		playerPosition=$(($playerPosition+$randomDiceNumber))
+		echo "Player position: $playerPosition"
+		;;
+	2)
+		echo "Snake"
+		echo "dice no.: $randomDiceNumber"
+		playerPosition=$(($playerPosition-$randomDiceNumber))
+		echo "Player position: $playerPosition"
+		;;	
+	*)
+		echo "No play"
+		playerPosition=$playerPosition
+		echo "Player position: $playerPosition"
+		;;
+esac
